2b2b3ecc8322fc88b0598782b411c779eb793114
Lokasenna <jalovatt@hotmail.com>
2018-10-31 13:39:48 -0600

    Added the ability to edit existing URLs

--- express_server.js
+++ express_server.js
@@ -50,6 +50,24 @@ app.post("/urls", (req, res) => {
 });
 
 
+app.get("/urls/:id", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  };
+  res.render("urls_show", templateVars);
+});
+
+app.post("/urls/:id", (req, res) => {
+
+  urlDatabase[req.params.id] = req.body.newURL;
+
+  var redirectURL = "http://localhost:8080/urls/";
+  res.redirect(redirectURL);
+});
+
+
+
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   var redirectURL = "http://localhost:8080/urls/";
